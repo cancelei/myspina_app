d20608b7f994e9a2fcc7b2ed056c8461356168b0
cancelei <glauber.bannwart@gmail.com>
2024-04-29 16:53:21 -0300

    Update application.js to remove unnecessary import and fix newline issue

---
+++ app/assets/config/spina/tailwind.config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  content: [
+    '/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/gems/spina-2.18.0/app/views/**/*.*',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/gems/spina-2.18.0/app/components/**/*.*',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/gems/spina-2.18.0/app/helpers/**/*.*',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/gems/spina-2.18.0/app/assets/javascripts/**/*.js',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/gems/spina-2.18.0/app/**/application.tailwind.css',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/layouts/default/application.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/admin/blog/categories/_category.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/admin/blog/categories/_form.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/admin/blog/categories/edit.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/admin/blog/categories/index.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/admin/blog/categories/new.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/admin/blog/posts/_form.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/admin/blog/posts/_form_post_configuration.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/admin/blog/posts/_form_post_content.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/admin/blog/posts/_form_post_seo.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/admin/blog/posts/_post.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/admin/blog/posts/edit.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/admin/blog/posts/index.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/admin/blog/posts/new.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/admin/hooks/blog/_primary_navigation.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/blog/categories/show.atom.builder',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/blog/categories/show.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/blog/comments/_comment.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/blog/comments/_form.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/blog/posts/_post.atom.builder',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/blog/posts/_post.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/blog/posts/archive.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/blog/posts/index.atom.builder',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/blog/posts/index.html.erb',
+'/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/app/views/spina/blog/posts/show.html.erb'
+  ],
+  theme: {
+    fontFamily: {
+      body: ['Metropolis'],
+      mono: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', "Liberation Mono", "Courier New", 'monospace']
+    },
+    extend: {
+      colors: {
+        spina: {
+          light: '#797ab8',
+          DEFAULT: '#6865b4',
+          dark: '#3a3a70'
+        }
+      }
+    }
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+	require('@tailwindcss/aspect-ratio'),
+	require('@tailwindcss/typography')
+  ]
+}
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,3 +1,4 @@
+// Unified Tailwind Configuration
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
@@ -5,19 +6,30 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
+    '/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/gems/spina-2.18.0/**/*.*',
+    '/home/glauber/.asdf/installs/ruby/3.2.2/lib/ruby/gems/3.2.0/bundler/gems/spina-blog-c5208f0a96e0/**/*.*'
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        body: ['Metropolis'],
+        mono: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', "Liberation Mono", "Courier New", 'monospace']
       },
-    },
+      colors: {
+        spina: {
+          light: '#797ab8',
+          DEFAULT: '#6865b4',
+          dark: '#3a3a70'
+        }
+      }
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
+    require('@tailwindcss/container-queries')
   ]
 }
